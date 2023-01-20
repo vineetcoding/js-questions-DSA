7e04657cd463432fa57787011f2005a77f72ac12
vineetcoding <114997519+vineetcoding@users.noreply.github.com>
2023-01-21 04:53:14 +0530

    work of map, filter, reduce function

--- mapFunction.js
+++ mapFunction.js
@@ -22,3 +22,72 @@ function binary(e) {
 }
 console.log(arr.map(binary))
 
+
+//============ use of Filter()=========================
+
+// filter odd value
+
+function odd(e){
+    return e % 2;
+}
+console.log(arr.filter(odd));
+
+//=============use of reduce()================
+
+
+// find sum in tradition method
+
+function findSum(arr){
+    let sum = 0;
+    for (let i=0; i<arr.length; i++){
+        sum+=arr[i];
+    }
+    return sum;
+}
+console.log(findSum(arr));
+
+
+// find sum with reduce() function
+
+const add = arr.reduce(function(add,curr){
+    return add += curr;
+},0);
+console.log(add);
+
+
+// find max with reduce() function
+
+const result = arr.reduce(function(max,curr){
+    if(curr>max){
+        max=curr;
+    }
+    return max;
+},0)
+console.log(result);
+
+// other real world example of reduce() function
+const users = [
+    {firstName:"akash", lastName:"singh", age:26},
+    {firstName:"gaurav", lastName:"tomer", age:75},
+    {firstName:"elon", lastName:"musk", age:50},
+    {firstName:"donald", lastName:"trump", age:26}
+];
+
+
+//count how many persons of every unique age
+
+const ans = users.reduce(function(acc, cur){
+    if(acc[cur.age]){
+        acc[cur.age]++;
+    }else{
+        acc[cur.age] = 1;
+    }
+    return acc;
+},{});
+console.log(ans);
+
+
+// print first name of every person which have age less then 30
+
+const answer = users.filter((x)=>x.age<30).map((y)=>y.firstName);
+console.log(answer);
